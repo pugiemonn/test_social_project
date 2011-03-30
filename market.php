81ab5dce0043e5b1f1325d3ddb548e935715a01c
pugiemonn <pugiemonn@gmail.com>
2011-03-30 18:00:32 +0900

    市場のファイルを追加

---
+++ market.php
@@ -0,0 +1,13 @@
+<?php
+require('libs/Smarty.class.php');
+
+$smarty = new Smarty;
+
+//$smarty->force_compile = true;
+$smarty->debugging = true;
+$smarty->caching = true;
+$smarty->cache_lifetime = 1;
+
+$smarty->assign("Name","Fred Irving Johnathan Bradley Peppergill",true);
+$smarty->display('market.tpl');
+?>
